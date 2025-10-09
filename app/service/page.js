d809c1a4bedd0938f566185abd838fa006a429b6
AnchalMish9630 <anchalmish2025@gmail.com>
2025-10-09 23:53:16 +0530

    Nested rotuing

---
+++ app/service/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Service () {
+  return (
+    <div>Service page
+        <Link href="/service/web-dev" >  Web dev</Link>
+    </div>
+  )
+}
